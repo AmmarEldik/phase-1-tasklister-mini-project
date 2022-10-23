9cd058b0b253363f8c317ed9aa92258f474a66ab
AmmarEldik <vektorea60@gmail.com>
2022-10-23 21:49:04 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+//document.addEventListener("DOMContentLoaded", () => {
+  // your code here 
+  //let x = document.querySelector("#create-task-form ");
+  //console.log(x);
+//});
+
+const taskForm = document.querySelector("#create-task-form")
+console.log(taskForm)
+const  taskList = document.querySelector("#tasks")
+taskForm.addEventListener("submit",function (e){
+  e.preventDefault();
+
+const newTask = document.querySelector("#new-task-description").value
+//const taskItem = document.createElement("li")
+//taskItem.innerText = newTask 
+taskList.innerHTML += `<li> ${newTask}
+<button data-action="delete"> x </button>
+</li> `
+taskForm.reset()
+//taskList.appendChild(taskItem)
+})
+
+taskList.addEventListener("click",function(e){
+  if (e.target.dataset.action === "delete"){
+    e.target.parentElement.remove()
+  }
+})
